e0699a528807390bc92dd791c62e8750bbb96248
Stephen Tong <stevetong83@hotmail.com>
2013-08-12 10:22:36 -0700

    active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Elbowroom::Application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   root to: 'home#index'
 
   resources :messages, only: [:new, :create]
